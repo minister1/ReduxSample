e84bdd4e253dfc18f3013a809f3ed5abb8e4f5ed
Sai Terala <sai.webm@gmail.com>
2016-07-27 19:34:09 -0700

    Intial Commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default () => {
+  return(
+    [
+      {title: "javascript: The Good Part"},
+      {title: 'Harry Potter'},
+      {title: 'The Dark Tower'},
+      {title: 'Ruby'}
+    ]
+  )
+}
